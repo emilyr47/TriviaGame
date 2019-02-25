12d730c9ad9599de5e6b7ee9bfc4de0115aef078
Emily Raper <emily@er-10-131-181-0.wireless.umn.edu>
2019-02-25 15:45:42 -0600

    Progress, still not working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,22 @@
-var counter = 60; // this is how much time they will have
+var counterP = 60; // this is how much time they will have
 var unit; // seconds
 var minus;
+
+$("#start").on("click", start);
+
 
 function start() {
   clearInterval(unit); // so that there can't be several times
   unit = setInterval(minus, 1000); // this allows the counter 60 to equal 60 seconds, not another unit
 }
 
+
 function perSecond() {
-  counter--; // decreasing counter by one per second
+  counterP--; // decreasing counter by one per second
 
-  $("#counter").html("<h2>You have " + counter + " seconds left </h2>"); //this is what will display the counter
+  $("#counter").html("You have " + counterP + " seconds left"); //this is what will display the counter
 
-  if (counter === 0) { // this is for when they run out of time
+  if (counterP === 0) { // this is for when they run out of time
     quit();
 
     alert("Your time is up!  Let's see how you did..."); // this will show up once they run out of time.  After this appears, it will show them the answers correct, wrong, and answered
